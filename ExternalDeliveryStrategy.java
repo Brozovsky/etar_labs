acc007687a7fe26e4897075ed50e2bb4eba7934e
Brozovsky <eup1670@gmail.com>
2023-05-07 21:40:17 +0300

    Delete ExternalDeliveryStrategy.java

--- ExternalDeliveryStrategy.java
+++
@@ -1,5 +0,0 @@
-public class ExternalDeliveryStrategy implements DeliveryStrategy {
-    public void calculateDeliveryCost() {
-        System.out.println("Calculating cost for external delivery service");
-    }
-}
