a4e9a9751866cb7aacc648a79dcb28c4668139b8
Brozovsky <eup1670@gmail.com>
2023-05-07 21:40:40 +0300

    Delete OwnDeliveryStrategy.java

--- OwnDeliveryStrategy.java
+++
@@ -1,5 +0,0 @@
-public class OwnDeliveryStrategy implements DeliveryStrategy {
-    public void calculateDeliveryCost() {
-        System.out.println("Calculating cost for own delivery service");
-    }
-}
