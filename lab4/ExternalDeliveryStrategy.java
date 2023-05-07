0c96623d944b9ed63f2aa947f317f601335a2bed
Brozovsky <eup1670@gmail.com>
2023-05-07 21:41:10 +0300

    Add files via upload

---
+++ lab4/ExternalDeliveryStrategy.java
@@ -0,0 +1,5 @@
+public class ExternalDeliveryStrategy implements DeliveryStrategy {
+    public void calculateDeliveryCost() {
+        System.out.println("Calculating cost for external delivery service");
+    }
+}
---
+++ lab4/OwnDeliveryStrategy.java
@@ -0,0 +1,5 @@
+public class OwnDeliveryStrategy implements DeliveryStrategy {
+    public void calculateDeliveryCost() {
+        System.out.println("Calculating cost for own delivery service");
+    }
+}
